7b2878b551de8bef2fe3749b17f2184fb7770798
CelyaRousseau <celyarousseau.pro@gmail.com>
2016-03-15 00:28:29 +0100

    improve(get real json links in output of redis)

--- app/controllers/LinksController.scala
+++ app/controllers/LinksController.scala
@@ -2,13 +2,16 @@ package controllers
 
 import javax.inject.Inject
 import domains.LinkRepository
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
 class LinksController @Inject()(linkRepository: LinkRepository) extends Controller {
 
   def findAll(limit: Int, offset: Int) = Action { request =>
-    Ok(linkRepository.getLinks().toString())
+    Ok(Json.toJson(linkRepository.getLinks()))
   }
 
   def find(tags: String) = play.mvc.Results.TODO
+
+  def insertAll() = play.mvc.Results.TODO
 }
--- app/domains/LinkRepository.scala
+++ app/domains/LinkRepository.scala
@@ -1,16 +1,24 @@
 package domains
 
 import javax.inject.Inject
+
+import com.fasterxml.jackson.databind.JsonNode
 import com.redis.RedisClientPool
+import play.libs.Json
 
 class LinkRepository @Inject() (pool: RedisClientPool)  {
 
-  def getLinks() : List[String] = {
+  def getLinks() : List[JsonNode] = {
     pool.withClient {
       client => {
-        val links : List[String] = client.zrange("links").get
-        links
+        client.zrange("links").get
       }
+    }.map { link =>
+      Json.parse(link)
     }
+  }
+
+  def addLinks(links :Json) = {
+    // TODO : Generify Links and push them in redis sortedSet [links]
   }
 }
