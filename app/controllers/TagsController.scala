3f176ca55a7a72338f7f1a6ca332b11bebe9fab5
CelyaRousseau <celyarousseau.pro@gmail.com>
2016-03-27 16:36:14 +0200

    fix(get /tags with their score)

--- app/controllers/TagsController.scala
+++ app/controllers/TagsController.scala
@@ -1,12 +1,13 @@
 package controllers
 
 import javax.inject.Inject
+import play.api.libs.json.Json
 import play.api.mvc._
 import domains.TagRepository
 
 class TagsController @Inject()(TagRepository: TagRepository) extends Controller {
 
   def findAll() = Action { request =>
-    Ok(TagRepository.findAll().toString())
+    Ok(Json.toJson(TagRepository.findAll()))
   }
 }
