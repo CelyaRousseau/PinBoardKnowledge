0b7b4f29d1ff1cc099f5b1e9118dc1a31b5b8d87
CelyaRousseau <celyarousseau.pro@gmail.com>
2016-04-02 18:28:10 +0200

    docs(LinkRepository) : add documentation

--- app/domains/LinkRepository.scala
+++ app/domains/LinkRepository.scala
@@ -4,13 +4,22 @@ import javax.inject.Inject
 
 import com.redis.RedisClientPool
 import org.joda.time.DateTime
-import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+import org.joda.time.format.DateTimeFormat
 import play.api.libs.json.{JsArray, JsValue, Json}
 
 class LinkRepository @Inject()(pool: RedisClientPool, tagRepository: TagRepository) {
 
-  def findAllWithParameters(limit: Int, offset: Int, q: Option[String], f: Option[String], intersect: Option[Boolean]): JsValue = {
-    val links: List[JsValue] = (q, f) match {
+  /**
+   * Find all Links according to parameters sent with pagination
+   *
+   * @param limit     Int     Limit for pagination
+   * @param offset    Int     Offset for pagination
+   * @param q         String  Query
+   * @param t         String  Tags
+   * @param intersect Boolean Boolean to set search with intersection
+   * @return
+   */
+  def findAllWithParameters(limit: Int, offset: Int, q: Option[String], t: Option[String], intersect: Option[Boolean]): JsValue = {
     val links: List[JsValue] = (q, t) match {
       case (None, None) => findAll
       case (Some(query), None) => findAllFilteredByQuery(query.split(",").toList)
@@ -24,6 +33,11 @@ class LinkRepository @Inject()(pool: RedisClientPool, tagRepository: TagReposito
     )
   }
 
+  /**
+   * Find all Links without any parameters
+   *
+   * @return
+   */
   def findAll: List[JsValue] = {
     pool.withClient {
       client => {
@@ -32,6 +46,13 @@ class LinkRepository @Inject()(pool: RedisClientPool, tagRepository: TagReposito
     }
   }
 
+  /**
+   * Find all links containing specific query from redis or links given
+   *
+   * @param query String                word(s) to search
+   * @param links Option[List[JsValue]] optionnal list of links
+   * @return
+   */
   def findAllFilteredByQuery(query: List[String], links: Option[List[JsValue]] = None): List[JsValue] = {
     val pattern = "(?i).*" + query.head + ".*" + query.tail.map(word => "|.*" + word + ".*").mkString
 
@@ -57,45 +78,58 @@ class LinkRepository @Inject()(pool: RedisClientPool, tagRepository: TagReposito
     }
   }
 
-  def findAllFilteredByTags(filters: List[String], intersect: Option[Boolean]): List[JsValue] = {
+  /**
+   * Find all links contained in tags with or without intersection
+   *
+   * @param tags      List[String]  List of tags
+   * @param intersect Boolean       Boolean to set search with intersection
+   * @return
+   */
+  def findAllFilteredByTags(tags: List[String], intersect: Option[Boolean]): List[JsValue] = {
     pool.withClient {
       client => {
-        val tags = filters.map { tag =>
+        val keys = tags.map { tag =>
           "tags:" + tag
         }
 
         intersect match {
-          case Some(true) => client.sinter(tags.head, tags.tail: _*)
-          case _ => client.sunion(tags.head, tags.tail: _*)
+          case Some(true) => client.sinter(keys.head, keys.tail: _*)
+          case _ => client.sunion(keys.head, keys.tail: _*)
         }
 
       }.get
     }.map(link => Json.parse(link.get)).toList
   }
 
-  def findAllFilteredByTagsAndQuery(query: String, filters: List[String], intersect: Option[Boolean]): List[JsValue] = {
-    pool.withClient {
-      client => {
-        val pattern = "(?i).*" + query + ".*"
-
-        findAllFilteredByTags(filters, intersect).collect({
-          case (link) if (link \ "description").as[String].matches(pattern)
-            || (link \ "extended").as[String].matches(pattern) => link
-        })
-      }
-    }
+  /**
+   * Find all links contained in filters with or without intersection and matching with query given
+   *
+   * @param query     String        word(s) to search
+   * @param tags      List[String]  List of tags
+   * @param intersect Boolean       Boolean to set search with intersection
+   * @return
+   */
   def findAllFilteredByTagsAndQuery(query: List[String], tags: List[String], intersect: Option[Boolean]): List[JsValue] = {
     findAllFilteredByQuery(query, Some(findAllFilteredByTags(tags, intersect)))
   }
 
+  /**
+   * Create links given in redis as well as create or update associated tags
+   *
+   * @param JSONLinks JsValue List Of Link
+   * @return
+   */
   def create(JSONLinks: JsValue) = {
-    val fmt: DateTimeFormatter = DateTimeFormat.forPattern("yyyy-dd-MM'T'HH:mm:ss'Z")
+    DateTimeFormat.forPattern("yyyy-dd-MM'T'HH:mm:ss'Z")
     var tags = collection.mutable.Map[String, Int]().withDefaultValue(0)
+
     pool.withClient {
       client => {
         JSONLinks.asInstanceOf[JsArray].value foreach { link =>
           val datetime: DateTime = new DateTime((link \ "time").as[String])
+
           client.zadd("links", datetime.getMillis, link).get
+
           (link \ "tags").as[String].split(" ").foreach { tag =>
             tags += (tag -> (tags.getOrElse(tag, 0) + 1))
             tagRepository.createLinkForTag(tag, link)
