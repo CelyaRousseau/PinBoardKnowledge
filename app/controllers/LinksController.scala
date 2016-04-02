9324c9ac820c3ee1b78a5e880304b0eb80f78faa
CelyaRousseau <celyarousseau.pro@gmail.com>
2016-04-02 18:49:56 +0200

    refactor(naming) : use tags instead of filters

--- app/controllers/LinksController.scala
+++ app/controllers/LinksController.scala
@@ -7,8 +7,8 @@ import play.api.mvc.{Action, Controller}
 
 class LinksController @Inject()(linkRepository: LinkRepository) extends Controller {
 
-  def findAll(limit: Int, offset: Int, filters: Option[String], q: Option[String], intersect: Option[Boolean]) = Action { request =>
-    Ok(Json.toJson(linkRepository.findAllWithParameters(limit, offset, q, filters, intersect)))
+  def findAll(limit: Int, offset: Int, tags: Option[String], q: Option[String], intersect: Option[Boolean]) = Action { request =>
+    Ok(Json.toJson(linkRepository.findAllWithParameters(limit, offset, q, tags, intersect)))
   }
 
   def create() = Action(parse.json) { request =>
